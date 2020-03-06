29e5886e84ebb905dba943cffc6abeb71703cbda
Antonio Piccolboni <antonio@piccolboni.info>
2020-03-05 23:48:33 -0800

    many more controls and reformatted code

--- app.R
+++ app.R
@@ -8,70 +8,154 @@
 #
 
 library(shiny)
-library(coronavirus)
+#devtools::install_github("RamiKrispin/coronavirus")
+#library(coronavirus)
 library(ggplot2)
 library(dplyr)
 library(directlabels)
 library(devtools)
 #devtools::install_github("tidyverse/tidyr")
 library(tidyr)
-# Define UI for application that draws a histogram
-ui <- fluidPage(
+library(httr)
 
-   # Application title
-   titlePanel("Daily cases per country, confirmed"),
 
-   # Sidebar with a slider input for number of bins
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("min_cases",
-                     "Minimum number of cases in one day for a country to be displayed",
-                     min = 5,
-                     max = 100,
-                     value = 10),
-         sliderInput("span",
-                     "Amount of smoothing to apply",
-                     min = 0,
-                     max = 1,
-                     value = 0.5)
+default = function(xx) {
+  if (length(xx) > 1) {
+    message(xx[[1]])
+    if (!is.null(xx[[1]]) && xx[[1]]) {
+      xx[[1]]
+    }
+    else {
+      default(xx[-1])
+    }
+  }
+  else {
+    xx[[1]]
+  }
+}
+
+unlink('coronavirus.rda')
+con = file('coronavirus.rda', 'wb')
+writeBin(content(
+  GET(url = 'https://raw.githubusercontent.com/RamiKrispin/coronavirus/master/data/coronavirus.rda')
+), con)
+close(con)
+load('coronavirus.rda')
+unlink('coronavirus.rda')
+
+ui <- fluidPage(# Application title
+  titlePanel(
+    paste(
+      "Daily cases per country, confirmed, updated:",
+      as.character(max(coronavirus$date))
+    )
+  ),
+
+  # Sidebar with a slider input for number of bins
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput(
+        "min_cases",
+        "Minimum number of cases in one day for a country to be displayed",
+        min = 1,
+        max = 100,
+        value = 10
       ),
-
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
+      dateRangeInput(
+        "date_range",
+        "Date range",
+        start = min(coronavirus$date),
+        end = max(coronavirus$date),
+        min = min(coronavirus$date),
+        max = max(coronavirus$date)
+      ),
+      radioButtons(
+        "type",
+        "Type",
+        choices = unique(coronavirus$type),
+        selected = "confirmed",
+        inline = TRUE
+      ),
+      uiOutput("country_selector"),
+      sliderInput(
+        "span",
+        "Amount of smoothing to apply (0 for no smoothing)",
+        min = 0,
+        max = 1,
+        value = 0.5
       )
-   )
-)
+    ),
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
+    # Show a plot of the generated distribution
+    mainPanel(plotOutput("distPlot"))
+  ))
 
-   output$distPlot <- renderPlot({
-     data = coronavirus %>%
-       group_by(Country.Region, type, date) %>%
-       summarise(cases = sum(cases)) %>%
-       filter(cases > 0, max(cases)>input$min_cases, type == "confirmed") %>%
-       arrange(date)
-     data = pivot_longer(data = pivot_wider(data = data, names_from =Country.Region, values_from = cases, values_fill = 0), cols = unique(data$Country.Region) , names_to = "Country.Region", values_to = "cases")
-     if(input$span == 0) {
-       main_geom = geom_line()}
-     else {
-       main_geom = geom_smooth(se = FALSE, span = input$span)}
-     ggplot(
-       data = data,
-       mapping = aes(
-         x=date,
-         y=cases,
-         label = Country.Region,
-         color=Country.Region)) +
-       main_geom +
-       geom_dl(method = "last.points")+
-       scale_y_log10()
-     })
+process_data = function(input) {
+  data = coronavirus %>%
+    group_by(Country.Region, type, date) %>%
+    summarise(cases = as.double(sum(cases))) %>%
+    filter(
+      cases > 0,
+      max(cases) > input$min_cases,
+      type == input$type,
+      date >= input$date_range[1],
+      date <= input$date_range[2]
+    ) %>%
+    arrange(date)
+  data = pivot_longer(
+    data = pivot_wider(
+      data = data,
+      names_from = Country.Region,
+      values_from = cases,
+      values_fill = 0.1
+    ),
+    cols = unique(data$Country.Region) ,
+    names_to = "Country.Region",
+    values_to = "cases"
+  )
+  countries = sort(unique(data$Country.Region))
+  list(data = filter(data, Country.Region %in% input$country),
+       countries = countries)
+}
+server <- function(input, output, session) {
+  countries = sort(unique(coronavirus$Country.Region))
+  output$distPlot <- renderPlot({
+    pd = process_data(input)
+    data = pd$data
+    countries = pd$countries
+    if (input$span == 0) {
+      main_geom = geom_line()
+    }
+    else {
+      main_geom = geom_smooth(se = FALSE, span = input$span)
+    }
+    ggplot(
+      data = data,
+      mapping = aes(
+        x = date,
+        y = cases,
+        label = Country.Region,
+        color = Country.Region
+      )
+    ) +
+      main_geom +
+      geom_dl(method = "last.bumpup") +
+      scale_y_log10()
+  }, height = 800)
+  output$country_selector = renderUI({
+    pd = process_data(input)
+    data = pd$data
+    countries = pd$countries
+    selectizeInput(
+      "country",
+      "Country",
+      choices = countries,
+      selected = if(is.null(input$country)){ countries} else {input$country},
+      multiple = TRUE
+    )
+  })
+
 }
 
 # Run the application
 shinyApp(ui = ui, server = server)
-
-
-
