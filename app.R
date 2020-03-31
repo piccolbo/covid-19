1fa94b32c6bd6aade2d26489c37c1ce625ee713a
Antonio Piccolboni <antonio@piccolboni.info>
2020-03-31 13:23:03 -0700

    major reorg splitting app and data processing

--- app.R
+++ app.R
@@ -7,40 +7,11 @@
 #    http://shiny.rstudio.com/
 #
 
-library(shiny)
+
+source("data_processing.R")
 library(ggplot2)
-library(dplyr)
 library(directlabels)
-library(devtools)
-library(tidyr)
-library(httr)
 library(DT)
-library(wbstats)
-
-
-corona = readr::read_csv("https://coronadatascraper.com/timeseries-tidy.csv",
-                         guess_max = 1000000)
-
-# corona = readr::read_csv("timeseries-tidy.csv.zip",
-#                          guess_max = 1000000)
-
-is_city = function(x) {
-  !is.na(x$city)
-}
-
-is_county = function(x) {
-  !is_city(x) & !is.na(x$county)
-}
-
-is_state = function(x) {
-  !is_city(x) & !is_county(x) & !is.na(x$state)
-}
-
-is_country = function(x) {
-  !is_city(x) & !is_county(x) & !is_state(x) & !is.na(x$country)
-}
-
-all_dates = unique(corona$date)
 
 options(DT.fillContainer = FALSE)
 options(DT.autoHideNavigation = FALSE)
@@ -48,7 +19,7 @@ options(DT.autoHideNavigation = FALSE)
 ui <- fluidPage(# Application title
   titlePanel(
     paste(
-      "Daily cases per country or state, confirmed, updated",
+      "Daily new cases (deaths, recovered) per country or state,  updated as of",
       as.character(max(all_dates))
     )
   ),
@@ -57,7 +28,7 @@ ui <- fluidPage(# Application title
     sidebarPanel(
       dateRangeInput(
         "date_range",
-        "Date range",
+        "Date range (default last three weeks)",
         start = max(all_dates) - 21,
         end = max(all_dates),
         min = min(all_dates),
@@ -71,10 +42,10 @@ ui <- fluidPage(# Application title
         inline = TRUE
       ),
       radioButtons(
-        "world_or_US",
-        "Show countries or US States",
-        choices = c("Countries", "US States"),
-        selected = "Countries",
+        "level",
+        "Choose admin unit level to visualize (US terminology used for all countries)",
+        choices = c("country", "state", "county"),
+        selected = "country",
         inline = TRUE
       ),
       radioButtons(
@@ -83,6 +54,7 @@ ui <- fluidPage(# Application title
         choices = c("no", "yes"),
         inline = TRUE
       ),
+      uiOutput("top_region_choice"),
       uiOutput("region_selector"),
       radioButtons(
         "smoothing",
@@ -92,191 +64,129 @@ ui <- fluidPage(# Application title
       )
     ),
 
-    mainPanel(tabsetPanel(
-      tabPanel("Time series", plotOutput("timeseries")),
-      tabPanel("Growth vs Size", plotOutput("growthvssize")),
-      tabPanel("Stats and projections", dataTableOutput("growth")),
-      tabPanel("Data", dataTableOutput("data")),
-      tabPanel(
-        "Methods and credits",
-        "Data from https://coronadatascraper.com/ Smoothed with https://stat.ethz.ch/R-manual/R-devel/library/stats/html/supsmu.html after a log transform (because the smoother is locally linear, and in log scale these trends appear locally linear) on the cumulative counts. Trends are computed with a linear model applied to the last 3 days of smoothed data in log scale (on the daily count, which I may reconsider). \"Doubling time\" is an estimate of how many days it takes for cases to double, negative numbers corresponding to decrease or halving time. Cases in 2 weeks is not a prediction, is to get an idea of what the current trend means, because most people don't get exponential growth. These choices are reasonable given a visual inspection of the data and the little I know about epidemiology but have not been validated and assume unchanged policies and attitudes in the affected countries, which is hopefully the wrong assumption, plus negligible levels of immunity in the population, which is correct but is bound to change in the future. I do have a background in science, but this has been hastly produced and not peer-reviewed. I which epidemiologists assisting governemnts communicated with the public in a way that makes this work irrelevant, but they don't at least in the US. You get soundbites at best. This analysis is meant to support the view that we are in an exponential phase of disesase spread in most countries and states, that is increase from one day to the next as a percentage is roughly constant. No health care system, let alone a system for tracking and isolating cases, can work more than a few weeks when cases double every 2 or 3 days as we are seeing in several countries as of early March. Only mobilizing a large fraction of the population can work (self-quarantine, social distancing, remote work, canceling gatherings, school closings, travel restrictions etc.). Countries that show a declining number of new cases (for example China, Hong Kong, South Korea as of early March) have applied these population-wide measures. Major caveat is that confirmed cases per day are capped by detection capacity for large outbreaks or in countries run by incompetent people and by censorship. In that case check the trends on number of deaths, which are harder to conceal -- but causes of death may be attributed incorrectly.  Code: https://github.com/piccolbo/covid-19 Feedback: covid19@piccolboni.info"
+    mainPanel(
+      tabsetPanel(
+        tabPanel("Time series", plotOutput("timeseries", height = "800px")),
+        tabPanel("Growth vs Size", plotOutput("growthvssize", height = "800px")),
+        tabPanel("Stats and projections", dataTableOutput("stats")),
+        tabPanel("Data", dataTableOutput("data")),
+        tabPanel(
+          "Methods and credits",
+          "Data from https://coronadatascraper.com/ Smoothed with https://stat.ethz.ch/R-manual/R-devel/library/stats/html/supsmu.html after a log transform (because the smoother is locally linear, and in log scale these trends appear locally linear) on the cumulative counts. Trends are computed with a linear model applied to the last 3 days of smoothed data in log scale (on the daily count, which I may reconsider). \"Doubling time\" is an estimate of how many days it takes for cases to double, negative numbers corresponding to decrease or halving time. Cases in 2 weeks is not a prediction, is to get an idea of what the current trend means, because most people don't get exponential growth. These choices are reasonable given a visual inspection of the data and the little I know about epidemiology but have not been validated and assume unchanged policies and attitudes in the affected countries, which is hopefully the wrong assumption, plus negligible levels of immunity in the population, which is correct but is bound to change in the future. I do have a background in science, but this has been hastly produced and not peer-reviewed. I which epidemiologists assisting governemnts communicated with the public in a way that makes this work irrelevant, but they don't at least in the US. You get soundbites at best. This analysis is meant to support the view that we are in an exponential phase of disesase spread in most countries and states, that is increase from one day to the next as a percentage is roughly constant. No health care system, let alone a system for tracking and isolating cases, can work more than a few weeks when cases double every 2 or 3 days as we are seeing in several countries as of early March. Only mobilizing a large fraction of the population can work (self-quarantine, social distancing, remote work, canceling gatherings, school closings, travel restrictions etc.). Countries that show a declining number of new cases (for example China, Hong Kong, South Korea as of early March) have applied these population-wide measures. Major caveat is that confirmed cases per day are capped by detection capacity for large outbreaks or in countries run by incompetent people and by censorship. In that case check the trends on number of deaths, which are harder to conceal -- but causes of death may be attributed incorrectly.  Code: https://github.com/piccolbo/covid-19 Feedback: covid19@piccolboni.info"
+        )
       )
-    ))
+    )
   ))
 
-decimal_trunc = function(x)
-  trunc(x * 100) / 100
-
-bottom = .1
-
-cdiff = function(x) {
-  pmax(0, c(bottom, diff(x)))
-}
-
-safe_approx = function(x,y, newx){
-  if (length(x) < 2)
-    list(x=x, y=y)
-  else
-    approx(x,y,newx)
-}
-
-fixed_length_smooth = function(x,y){
-  ss = supsmu(x,y)
-  safe_approx(ss$x, ss$y, x)
-}
-
-logsmooth = function(x) {
-  y = pmax(bottom, x)
-  pmax(bottom, exp(fixed_length_smooth(1:length(y), log(y))$y))
-}
-
-safe_log2 = function(x)
-  log2(pmax(x, bottom))
-
-spy = function(x, f) {
-  print(do.call(paste, as.list(f(x))))
-  x
-}
-
-df2named_vector = function(keys, values, data = NULL) {
-  if (!is.null(data)) {
-    keys = data[[keys]]
-    values = data[[values]]
+safe_select_regions = function(regions, data) {
+  if (is.null(regions) ||
+      !all(regions %in% data$region)) {
+    high_cases_regions(data)
+  } else {
+    regions
   }
-  v = values
-  names(v) = keys
-  v
 }
-
-three_letter2country = function(x) {
-  map = df2named_vector("Alpha_3", "Name", ISOcodes::ISO_3166_1)
-  unlist(map[x])
-}
-
-
-process_data = function(world, type) {
-  tp = type
-  data = corona %>%
-    mutate(country = three_letter2country(country)) %>%
-    filter(type  == tp)
-
-  data = if (world) {
-    rename(data, region = "country")
-  }
-  else{
-    filter(data, is_state(data) & country == "United States") %>%
-      rename(region = "state")
-  }
-
-
-  group_by(data, region) %>%
-    arrange(date) %>%
-    rename(cumulative.value = value) %>%
-    mutate(smoothed.cumulative.value = logsmooth(cumulative.value)) %>%
-    mutate(
-      increase = cdiff(cumulative.value),
-      smoothed.increase = cdiff(smoothed.cumulative.value)
-    ) %>%
-    mutate(
-      log2.cumulative.value = safe_log2(cumulative.value),
-      log2.smoothed.cumulative.value = safe_log2(smoothed.cumulative.value),
-      log2.increase = safe_log2(increase),
-      log2.smoothed.increase = safe_log2(smoothed.increase)
-    )
-
-
-}
-
-high_cases_regions = function(data) {
-  (data %>%
-     filter(date == max(date)) %>%
-     group_by(region) %>%
-     arrange(-smoothed.increase) %>%
-     head(12))$region
-}
-
-filter_regions = function(data, regions) {
-  filter(data,
-         region %in% regions)
-}
-
-trend_calc = function(data) {
-  n_days_ago = tail(sort(unique(data$date)), 3)[1]
-  last_day = max(data$date)
-
-  data %>%
-    filter(date >= n_days_ago, date <= last_day) %>%
-    group_by(region) %>%
-    arrange(date) %>%
-    summarize(
-      model1 = list(lm(formula = log2.smoothed.increase ~ date)),
-      # model2 = list(lm(formula = log2value ~ poly(date, 2))),
-      log2.latest.value = last(log2.smoothed.increase),
-      latest.cumulative.value = last(cumulative.value),
-      population = last(population)
-    ) %>%
-    mutate(
-      log2.growth.rate = sapply(model1, function(x)
-        x$coeff[2]),
-      growth.rate = 2 ** log2.growth.rate,
-      doubling.time = decimal_trunc(1 / log2.growth.rate)#,
-      #concavity = sapply(model2, function(x)        x$coeff[3])
-      # pval = sapply(model2, function(x) (x %>% summary %>% coefficients)[3,4])
-    ) %>%
-    mutate(in.15.days = sapply(model1, function(x)
-      trunc(2 ** predict(
-        x, newdata = data.frame(date = last_day + 15)
-      )))) %>%
-    mutate(
-      daily.growth.percent = trunc((growth.rate - 1) * 100),
-      latest.value = trunc(2 ** log2.latest.value),
-      latest.value.per.hundred.thousand =
-        decimal_trunc(latest.value * 1E5 / population)
-    ) %>%
-    dplyr::select(-starts_with("model"),
-                  -starts_with("log2"),
-                  -growth.rate,
-                  -population)
-}
-
 
 server <- function(input, output, session) {
-  reset_selection = FALSE
-  output$timeseries = renderPlot({
-    data = process_data(input$world_or_US == "Countries", input$type) %>%
-      filter(date >= input$date_range[1],
-             date <= input$date_range[2]) %>%
-      filter_regions(input$regions)
-    plot = ggplot(
-      data = data,
-      mapping = aes(
-        x = date,
-        y = (if (input$smoothing == "yes")
-          smoothed.increase
-          else
-            increase) / (if (input$density == "yes")
-              population / 1E5
-          else
-            1),
-        label = region,
-        color = region
+  output$top_region_choice = renderUI({
+    if (input$level != "country") {
+      data = process_data(
+        data = corona,
+        level = one_level_up(input$level),
+        type = input$type,
+        date_range = input$date_range
       )
-    ) +
-      geom_line() +
-      geom_dl(method = "angled.boxes") +
-      scale_y_log10(labels = identity) +
-      ylab(paste(input$type, (if (input$density == "yes")
-        "per hundred thousands"
-        else
-          ""))) +
-      theme(legend.position = "none")
-    plot
-  }, height = 800)
-  output$growthvssize = renderPlot({
-    data = process_data(input$world_or_US == "Countries", input$type) %>%
-      filter(date >= input$date_range[1],
-             date <= input$date_range[2]) %>%
-      filter_regions(input$regions)
+      all_regions = sort(unique(data$region))
+      selectizeInput(
+        "top_region",
+        "Focus on this specific region",
+        choices = all_regions,
+        selected = safe_select_regions(input$region, data)[1],
+        multiple = FALSE
+      )
+    }
+  })
+  output$region_selector = renderUI({
+    data = process_data(
+      data = corona,
+      level = input$level,
+      type = input$type,
+      top_region = input$top_region,
+      date_range = input$date_range
+    )
+    all_regions = sort(unique(data$region))
+    selectizeInput(
+      "regions",
+      "Regions to show data from (biggest current outbreaks shown, click for more)",
+      choices = all_regions,
+      selected = safe_select_regions(input$regions, data),
+      multiple = TRUE
+    )
+  })
+
+  output$timeseries = renderCachedPlot({
+    message(paste(
+      c(input$level,
+        input$type,
+        input$top_region,
+        input$regions),
+      collapse = " | "
+    ))
+    if (!is.null(input$regions)) {
+      data = process_data(
+        data = corona,
+        level = input$level,
+        type = input$type,
+        top_region = input$top_region,
+        regions = input$regions,
+        date_range = input$date_range
+      )
+      ggplot(
+        data = data,
+        mapping = aes(
+          x = date,
+          y = (if (input$smoothing == "yes")
+            smoothed.increase
+            else
+              increase) / (if (input$density == "yes")
+                population / 1E5
+                else
+                  1),
+          label = region,
+          color = region
+        )
+      ) +
+        geom_line() +
+        geom_dl(method = "angled.boxes") +
+        scale_y_log10(labels = identity) +
+        ylab(paste("New daily", input$type, (if (input$density == "yes")
+          "per hundred thousands"
+          else
+            ""))) +
+        theme(legend.position = "none")
+    }
+  },
+  cacheKeyExpr = list(
+    input$regions,
+    input$smoothing,
+    input$date_range,
+    input$density,
+    input$type,
+    strsplit(date(), " ")[[1]][1:3]
+  ))
+  output$growthvssize = renderCachedPlot({
+    data = process_data(
+      data = corona,
+      level = input$level,
+      type = input$type,
+      top_region = input$top_region,
+      regions = input$regions#,
+      # date_range = input$date_range
+    )
+    min.cumu.value = if (input$density == "yes")
+      0.03
+    else
+      100
     plot = ggplot(
       data = data,
       mapping = aes(
@@ -284,44 +194,55 @@ server <- function(input, output, session) {
           population / 1E5
           else
             1),
-        y = decimal_trunc(smoothed.increase / smoothed.cumulative.value),
+        y = 100 * decimal_trunc(
+          ifelse(
+            smoothed.cumulative.value > min.cumu.value,
+            smoothed.increase / smoothed.cumulative.value,
+            NA
+          )
+        ),
         label = region,
         color = region
       )
     ) +
       geom_line() +
-      geom_point() +
       geom_dl(method = "angled.boxes") +
-      scale_x_log10(labels = identity) +
+      scale_x_log10(labels = identity, limits = c(min.cumu.value, NA)) +
       scale_y_log10(labels = identity) +
       xlab(paste(input$type, (if (input$density == "yes")
         "per hundred thousands"
         else
           ""))) +
-      ylab("growth rate")+
+      ylab("growth rate") +
       theme(legend.position = "none")
     plot
-  }, height = 800)
-  output$growth = renderDataTable({
-    trend_calc(process_data(input$world_or_US == "Countries", input$type))
+  },
+  cacheKeyExpr = list(
+    input$regions,
+    input$smoothing,
+    input$density,
+    input$type,
+    strsplit(date(), " ")[[1]][1:3]
+  ))
+  output$stats = renderDataTable({
+    trend_calc(
+      data = process_data(
+        data = corona,
+        level = input$level,
+        type = input$type,
+        top_region = input$top_region,
+        date_range = input$date_range
+      )
+    )
   }, options = list(order = list(list(5, 'desc'))))
   output$data = renderDataTable({
-    process_data(input$world_or_US == "Countries", input$type)
-  })
-  output$region_selector = renderUI({
-    data = process_data(input$world_or_US == "Countries", input$type)
-    all_regions = sort(unique(data$region))
-    selectizeInput(
-      "regions",
-      "Region (biggest current outbreaks shown, click for more)",
-      choices = all_regions,
-      selected = if (is.null(input$regions) ||
-                     !all(input$regions %in% all_regions)) {
-        high_cases_regions(data)
-      } else {
-        input$regions
-      },
-      multiple = TRUE
+    data = process_data(
+      data = corona,
+      level = input$level,
+      type = input$type,
+      top_region = input$top_region,
+      regions = input$regions,
+      date_range = input$date_range
     )
   })
 }
---
+++ data_processing.R
@@ -0,0 +1,242 @@
+library(readr)
+library(dplyr)
+
+corona = readr::read_csv(
+  "https://coronadatascraper.com/timeseries-tidy.csv",
+  col_types = cols(
+    city = col_character(),
+    county = col_character(),
+    state = col_character(),
+    country = col_character(),
+    population = col_double(),
+    lat = col_double(),
+    long = col_double(),
+    url = col_character(),
+    aggregate = col_character(),
+    tz = col_character(),
+    date = col_date(format = ""),
+    type = col_character(),
+    value = col_double()
+  )
+)
+
+# filtering
+
+is_city = function(x) {
+  !is.na(x$city)
+}
+
+is_county = function(x) {
+  !is_city(x) & !is.na(x$county)
+}
+
+is_state = function(x) {
+  !is_city(x) & !is_county(x) & !is.na(x$state)
+}
+
+is_country = function(x) {
+  !is_city(x) & !is_county(x) & !is_state(x) & !is.na(x$country)
+}
+
+all_dates = unique(corona$date)
+all_types = unique(corona$type)
+
+
+
+
+filter_regions = function(data, regions, level) {
+  filter_(data,
+          paste(level, " %in% regions"))
+}
+
+
+all_levels = c("city", "county", "state", "country")
+
+one_level_up = function(level) {
+  all_levels[which(all_levels == level) + 1]
+}
+
+filter_data = function(data,
+                       level = all_levels,
+                       type = c(all_types),
+                       top_region = NULL,
+                       regions = NULL,
+                       date_range = NULL) {
+  level = match.arg(level)
+  top_level = one_level_up(level)
+  typ = match.arg(type)
+  level_filter = list(
+    city = is_city,
+    county = is_county,
+    state = is_state,
+    country = is_country
+  )[[level]]
+  data = filter(data, level_filter(data), type == typ)
+  if (!is.null(top_region)) {
+    data = filter_(data, paste(top_level,  " == top_region"))
+  }
+  if (!is.null(regions)) {
+    data = filter_regions(data, regions, level)
+  }
+  if (!is.null(date_range))
+    data = filter(data, date >= date_range[1] - 1, #1 extra for diffs
+                  date <= date_range[2])
+  data
+}
+
+
+df2named_vector = function(keys, values, data = NULL) {
+  if (!is.null(data)) {
+    keys = data[[keys]]
+    values = data[[values]]
+  }
+  v = values
+  names(v) = keys
+  v
+}
+
+three_letter2country = function(x) {
+  map = df2named_vector("Alpha_3", "Name", ISOcodes::ISO_3166_1)
+  unlist(map[x])
+}
+
+expand_country_names = function(data) {
+  mutate(data, country = three_letter2country(country))
+}
+
+set_region = function(data,
+                      level = c("city", "county", "state", "country")) {
+  level = match.arg(level)
+  rename(data, region = level)
+}
+
+
+decimal_trunc = function(x)
+  trunc(x * 100) / 100
+
+bottom = .1
+
+cdiff = function(x) {
+  #this is wrong
+  pmax(0, c(bottom, diff(x)))
+}
+
+safe_approx = function(x, y, newx) {
+  if (length(x) < 2)
+    list(x = x, y = y)
+  else
+    approx(x, y, newx)
+}
+
+fixed_length_smooth = function(x, y) {
+  ss = supsmu(x, y)
+  safe_approx(ss$x, ss$y, x)
+}
+
+logsmooth = function(x) {
+  y = pmax(bottom, x)
+  pmax(bottom, exp(fixed_length_smooth(1:length(y), log(y))$y))
+}
+
+safe_log2 = function(x)
+  log2(pmax(x, bottom))
+
+spy = function(x, f) {
+  print(do.call(paste, as.list(f(x))))
+  x
+}
+
+
+more_columns = function(data) {
+  group_by(data, region) %>%
+    arrange(date) %>%
+    rename(cumulative.value = value) %>%
+    mutate(smoothed.cumulative.value = logsmooth(cumulative.value)) %>%
+    mutate(
+      increase = cdiff(cumulative.value),
+      smoothed.increase = cdiff(smoothed.cumulative.value)
+    ) %>%
+    mutate(
+      log2.cumulative.value = safe_log2(cumulative.value),
+      log2.smoothed.cumulative.value = safe_log2(smoothed.cumulative.value),
+      log2.increase = safe_log2(increase),
+      log2.smoothed.increase = safe_log2(smoothed.increase)
+    ) %>%
+    filter(date > min(date))
+}
+
+high_cases_regions = function(data) {
+  (
+    data %>%
+      filter(date == max(date)) %>%
+      group_by(region) %>%
+      arrange(-smoothed.increase) %>%
+      head(12)
+  )$region
+}
+
+arg_consistency =function(data, level, top_region, regions){
+
+}
+process_data = function(data,
+                        level = c("city", "county", "state", "country"),
+                        type = c(all_types),
+                        top_region = NULL,
+                        regions = NULL,
+                        date_range = NULL) {
+  level = match.arg(level)
+  type = match.arg(type)
+  data = expand_country_names(data)
+  message(paste(c(level, type, top_region, regions)), collapse = "|")
+  filter_data(
+    data,
+    level = level,
+    type = type,
+    top_region = top_region,
+    regions = regions,
+    date_range = date_range
+  ) %>%
+    set_region(level = level) %>%
+    more_columns()
+}
+
+trend_calc = function(data) {
+  n_days_ago = tail(sort(unique(data$date)), 3)[1]
+  last_day = max(data$date)
+
+  data %>%
+    filter(date >= n_days_ago, date <= last_day) %>%
+    group_by(region) %>%
+    arrange(date) %>%
+    summarize(
+      model1 = list(lm(formula = log2.smoothed.increase ~ date)),
+      # model2 = list(lm(formula = log2value ~ poly(date, 2))),
+      log2.latest.value = last(log2.smoothed.increase),
+      latest.cumulative.value = last(cumulative.value),
+      population = last(population)
+    ) %>%
+    mutate(
+      log2.growth.rate = sapply(model1, function(x)
+        x$coeff[2]),
+      growth.rate = 2 ** log2.growth.rate,
+      doubling.time = decimal_trunc(1 / log2.growth.rate)#,
+      #concavity = sapply(model2, function(x)        x$coeff[3])
+      # pval = sapply(model2, function(x) (x %>% summary %>% coefficients)[3,4])
+    ) %>%
+    mutate(in.15.days = sapply(model1, function(x)
+      trunc(2 ** predict(
+        x, newdata = data.frame(date = last_day + 15)
+      )))) %>%
+    mutate(
+      daily.growth.percent = trunc((growth.rate - 1) * 100),
+      latest.value = trunc(2 ** log2.latest.value),
+      latest.value.per.hundred.thousand =
+        decimal_trunc(latest.value * 1E5 / population)
+    ) %>%
+    dplyr::select(-starts_with("model"),
+                  -starts_with("log2"),
+                  -growth.rate,
+                  -population)
+}
+
+
